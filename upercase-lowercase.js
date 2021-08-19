e226d22e6f90aaa193508deec8b3bfe82a6fe14b
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-08-19 12:20:50 +0600

    array string add

---
+++ upercase-lowercase.js
@@ -0,0 +1,9 @@
+const userMail ='saBbirnahid5817@gmail.com';
+const lowerlitter =userMail.toLowerCase();
+
+if(userMail == 'sabbirnaJid5817@gmail.com'){
+    console.log(lowerlitter);
+}
+else{
+    console.log('error 404');
+}
